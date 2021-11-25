72f60c3f32bc61847438b432483a6a3685b304fb
Ievgen Dimov <ievgen7@gmail.com>
2021-11-24 22:09:45 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+const menuBtn = document.querySelector('.menu-btn')
+const mainMenu = document.querySelector('.main-menu')
+
+menuBtn.addEventListener('click', () => {
+    mainMenu.classList.toggle('show')
+});
